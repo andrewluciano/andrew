65b86d7931c2b820d5b781176a97a27ff097129c
Andrew Silva <andrew.luciano@gmail.com>
2025-08-19 12:39:44 -0300

    tooltip

--- js/func.js
+++ js/func.js
@@ -1,3 +1,17 @@
-$(function() {
-    $("img").lazyload();
-});
+$(function(){
+    $( document ).tooltip({
+      position: {
+        my: "center bottom-20",
+        at: "center top",
+        using: function( position, feedback ) {
+          $( this ).css( position );
+          $( "<div>" )
+            .addClass( "arrow" )
+            .addClass( feedback.vertical )
+            .addClass( feedback.horizontal )
+            .appendTo( this );
+        }
+      }
+    });
+    $("img").lazyload()})
+;
